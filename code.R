3ec4959c6aadb247cbd21a2ad475fd3fc19f6f0c
nfj1380 <nfountainjones@gmail.com>
2020-04-14 17:24:05 +1000

    Updated code, tree files and alignment

---
+++ code.R
@@ -0,0 +1,264 @@
+#COde to analyse differences in covid19 genomes
+
+#Written by Nick Fountain-Jones (nfj@umn.edu)
+
+library(treeio)
+library(ggtree)
+library(tidyverse)
+library(treestructure) 
+library(phylodyn)# needs to options(buildtools.check = function(action) TRUE ) before installing from Git
+library(skygrowth)
+library(coda)
+library(ggmcmc)
+
+#------------------------------------------------------------------------
+##################Visualize MCC Tree##################
+#------------------------------------------------------------------------
+#import .tre file
+beast <- read.beast('covid_ExpClock1_test3.treReduced3.txt')
+
+#can read nexus too
+b <-  read.nexus('BEASTtreeApril2019.tree')
+
+
+#can see node numbers
+ph <- ggtree(beast) + geom_tiplab(size=2)+ geom_text2(aes(subset=!isTip, label=node), hjust=-.3) 
+ph  
+
+#add branch lengths etc
+p1 <- ggtree(beast , mrsd="2020-04-04") + theme_tree2()+ 
+  #geom_tiplab(align=FALSE, linetype='dashed', linesize=0.5, size=1) +  
+  geom_range("length_0.95_HPD", color='red', size=2, alpha=.5, nodes=730) + #gets length estimates 
+  geom_text2(aes(label=round(as.numeric(posterior), 2), 
+                 subset=as.numeric(posterior)> 0.9, 
+                 x=branch), vjust=0)
+p1
+
+#can isolate particular sections of the tree
+viewClade(p1+geom_tiplab(size=2), node=730)
+
+
+#------------------------------------------------------------------------
+##################look for non-random tree structure using treestructure (Volz et al)##################
+#------------------------------------------------------------------------
+
+#try min 10 for clade size 
+treeSt <-  trestruct(b, minCladeSize = 100, minOverlap = -Inf, nsim = 10000, #100000 didn't change anythin
+                     level = 0.05, ncpu = 1, verbosity = 1) 
+#20 gets 12 groups - too many?
+  #50-200 stable 3 groups
+treeSt_df <- as.data.frame(treeSt)
+
+plot(treeSt, use_ggtree = TRUE)
+
+#----------------------------
+#subset tree - went with three groups 
+#---------------------------
+Clade1 <- tree_subset(beast, node=730,levels_back = 0)
+Clade1nex <- tree_subset(b, node=730,levels_back = 0) #useful for analyses below
+
+#basic tree
+ggtree(Clade1) + geom_tiplab(size=2)+ geom_text2(aes(subset=!isTip, label=node), hjust=-.3) 
+#more annotation added
+pc1 <- ggtree(Clade1, mrsd="2020-04-04") + theme_tree2()+ 
+  geom_tiplab(align=FALSE, linetype='dashed', linesize=0.5, size=1) +  
+  geom_range("length_0.95_HPD", color='red', size=2, alpha=.5, nodes=730) + #gets length estimates 
+  geom_text2(aes(label=round(as.numeric(posterior), 2), 
+                 subset=as.numeric(posterior)> 0.9, 
+                 x=branch), vjust=0)
+pc1
+
+Clade1_names <- as.data.frame(get_taxa_name(tree_view = NULL, node = NULL))
+Clade1_names$seq <- as.factor(Clade1_names$seq)
+
+Clade1_names$seq <- gsub(("'"), (""), Clade1_names$seq)
+
+gsub("%", "", as.character(factor("14.9%")) )
+
+Clade2<- tree_subset(beast , node=594,levels_back = 0)
+Clade2nex <- tree_subset(b, node=594,levels_back = 0) #useful for analyses below
+
+# going forward in the tree to remove weird outliers
+ggtree(Clade2) + geom_tiplab(size=2)+ geom_text2(aes(subset=!isTip, label=node), hjust=-.3) 
+
+pc2 <- ggtree(Clade2, mrsd="2020-04-04") + theme_tree2()+ 
+  geom_tiplab(align=FALSE, linetype='dashed', linesize=0.5, size=1) +  
+  geom_range("length_0.95_HPD", color='red', size=2, alpha=.5, nodes=730) + #gets length estimates 
+  geom_text2(aes(label=round(as.numeric(posterior), 2), 
+                 subset=as.numeric(posterior)> 0.9, 
+                 x=branch), vjust=0)
+pc2
+
+Clade2_names <- as.data.frame(get_taxa_name(tree_view = NULL, node = NULL))
+
+Clade3 <- tree_subset(beast , node=820,levels_back = 0)
+Clade3nex <- tree_subset(b, node=820,levels_back = 0)#useful for analyses below
+
+ggtree(Clade3) + geom_tiplab(size=2)+ geom_text2(aes(subset=!isTip, label=node), hjust=-.3) #complete tree
+
+pc3 <- ggtree(Clade3, mrsd="2020-04-04") + theme_tree2()+ 
+  geom_tiplab(align=FALSE, linetype='dashed', linesize=0.5, size=1) +  
+  geom_range("length_0.95_HPD", color='red', size=2, alpha=.5, nodes=730) + #gets length estimates 
+  geom_text2(aes(label=round(as.numeric(posterior), 2), 
+                 subset=as.numeric(posterior)> 0.9, 
+                 x=branch), vjust=0)
+pc3
+
+Clade3_names <- as.data.frame(get_taxa_name(tree_view = NULL, node = NULL))
+
+
+#----------------------------
+#Effective population size for each clade
+#---------------------------
+
+#overall - all clades included. lengthout means ne changes every 3 days or so
+
+GlobalBSP <- BNPR(b, lengthout = 35, prec_alpha = 0.01, prec_beta = 0.01,
+                   beta1_prec = 0.001, fns = NULL, log_fns = TRUE, simplify = TRUE,
+                   derivative = FALSE, forward = TRUE)
+
+plot_BNPR(GlobalBSP)
+
+#ps helps account for preferential sampling -not needed in this case as sampling has been consistent enough?
+
+BSpsClade1<- BNPR_PS(Clade1nex, lengthout = 35, prec_alpha = 0.01, prec_beta = 0.01, #lenghout
+                     beta1_prec = 0.001, fns = NULL, log_fns = TRUE, simplify = TRUE,
+                     derivative = FALSE, forward = TRUE) #with pref sampling
+BSpsClade1a<- BNPR(Clade1nex, lengthout = 35, prec_alpha = 0.01, prec_beta = 0.01,
+                   beta1_prec = 0.001, fns = NULL, log_fns = TRUE, simplify = TRUE,
+                   derivative = FALSE, forward = TRUE)
+
+plot_BNPR(BSpsClade1)
+plot_BNPR(BSpsClade1a)
+
+BSpsClade2a<- BNPR(Clade2nex, lengthout = 35, prec_alpha = 0.01, prec_beta = 0.01, #lengthout = number of grid points
+                     beta1_prec = 0.001, fns = NULL, log_fns = TRUE, simplify = TRUE,
+                     derivative = FALSE, forward = TRUE)
+
+plot_BNPR(BSpsClade2a)
+
+BSpsClade3a<- BNPR(Clade3nex, lengthout = 35, prec_alpha = 0.01, prec_beta = 0.01,
+                     beta1_prec = 0.001, fns = NULL, log_fns = TRUE, simplify = TRUE,
+                     derivative = FALSE, forward = TRUE)
+
+plot_BNPR(BSpsClade3a)
+
+
+# skygrowth BP
+fitC1 <- skygrowth.map(Clade1nex,  
+                     , res = 7*5  # Ne changes every 3 days or so?
+                     , tau0 = .1    # Smoothing parameter. If prior is not specified, this will also set the scale of the prior
+)
+plot(fitC1)
+growth.plot(fitC1)+theme_bw()
+
+fitC2 <- skygrowth.map(Clade2,  
+                       , res = 7*5  # Ne changes every 3 days or so? Doesn't change much.
+                       , tau0 = .1    # Smoothing parameter. If prior is not specified, this will also set the scale of the prior
+)
+plot(fitC2)
+growth.plot(fitC2)
+
+fitC3 <- skygrowth.map(Clade3,  
+                       , res = 7*5  # Ne changes every 3 days or so? Doesn't change much.
+                       , tau0 = .1    # Smoothing parameter. If prior is not specified, this will also set the scale of the prior
+)
+plot(fitC3)
+growth.plot(fitC3)
+
+#fit with mcmc - I get a different result here than with skygrid map. I'm laos struggling to check convergence
+mcmcfit_c1 <- skygrowth.mcmc(Clade1nex, res = 35, tau0=.1, mhsteps= 1e+06 ) #maybe 10 million?
+growth.plot( mcmcfit_c1 )+theme_bw()
+
+#check mcmc diagnostics - not working as yet - not sure what parameters to assess in mcmcfit_c1 
+#a1 <- as.mcmc(as.data.frnot sure ame(mcmcfit_c1$growthrate))
+#effectiveSize(a1)#time series length N, square  root{var x)}/n
+
+#make a ggmcmc object
+#ggA <- ggs(a1)
+#HPDinterval(a1)
+#ggs_density(ggA)
+#ggs_traceplot(ggA)
+
+
+mcmcfit_c2 <- skygrowth.mcmc(Clade2nex, res = 35, tau0=.1, mhsteps= 1e+05 ) #not sure how to tune this
+growth.plot( mcmcfit_c2 )+theme_bw()
+
+mcmcfit_c3 <- skygrowth.mcmc(Clade3nex, res = 35, tau0=.1, mhsteps= 1e+05 ) #not sure how to tune this
+growth.plot( mcmcfit_c3 )+theme_bw()
+
+str(mcmcfit_c3)
+
+#----------------------------
+#Extracting sequences from each clade
+#---------------------------
+library(ggmsa)
+library(Biostrings) 
+library(DECIPHER) 
+library(tidyverse)
+library(ape)
+
+#BiocManager::install("DECIPHER") #etc
+dna <- readDNAStringSet("covid_alignment.fasta")
+
+dnadf <- as.data.frame(readDNAStringSet("covid_alignment.fasta"))
+
+dnaNN <- tibble::rownames_to_column(dnadf, "seq")
+
+
+clade_1seq <- dnaNN %>% 
+  filter( dnaNN$seq %in% Clade1_names$seq )
+
+
+row.names(clade_1seq) <- as.character(clade_1seq$seq)
+ SeqNames <- clade_1seq$seq
+clade_1seq$seq <- NULL
+
+names(clade_1seq) <- 'dna'
+clade_1seqMat <- as.matrix(clade_1seq)
+
+
+clade1seqB <-DNAStringSet(clade_1seqMat, use.names=TRUE)
+clade1seqB@ranges@NAMES <- SeqNames
+
+#make it a string again! problem here with the names
+clade_1seqMatNames <- cbind(clade1seqB,  SeqNames)
+
+library(tigger)
+writeFasta(clade1seqB, file= "clade1seq.fasta ")
+#re-align - not needed
+
+clade_1seqNG <- RemoveGaps(clade1seqB , "all")# why it wants gaps removed?
+alignedC1 <- AlignSeqs(clade_1seqNG)
+
+# view the alignment in a browser (optional)
+BrowseSeqs(alignedC1, highlight=0) #awful alignment. 
+
+#----------------------------
+#Selection using CorMut/Ape
+#---------------------------
+
+# dNdS - works but not sure how to sue the reusltant matrix
+#convert to right format
+clade1dN <- as.DNAbin(clade1seqB ) #may need to add a extra consensus sequence.
+
+C1dnds <-  dnds( clade1dN  , code = 1, codonstart = 1, quiet = FALSE)
+
+
+library(CorMut) #doesn't work as failing to load. Moving to FUBAR in datamonkey.
+file <- system.file("clade1seq.fasta",package="CorMut")
+
+file <- system.file('C:/Users/Nick FJ/Documents/Covid19/clade1seq.fasta')
+
+clade1dN <- seqFormat(file, format = c("fasta"))
+
+result <- kaksCodon(clade1seqB)
+
+fresult <-  filterSites(result)
+head(fresult)
+
+
+#useful code  
+ex.dna <- read.dna("covid_alignment.fasta", format = "fasta")
+names(ex.dna)
+ggmsa(ex.dna) , color = "Chemistry_NT")
